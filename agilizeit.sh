7ef9e7b8413b626545801065a167ee2a8eac2227
Luiz Nickel <luizaugustonickel@gmail.com>
2017-01-30 14:00:23 -0300

    0.2 - More update, added docker and compose, maven, java, php,terminator

--- agilizeit.sh
+++ agilizeit.sh
@@ -23,31 +23,55 @@ echo "Now installing Git and Gitflow"
 sleep 2
 sudo apt install -y git && sudo apt install -y git-flow
 
-echo "Configuring SSH (TBD)"
+echo "Installing Docker and Docker Compose"
 sleep 2
-# TODO
+echo "First... Docker Engine!"
+sleep 1
+sudo apt-get install -y curl \
+        linux-image-extra-$(uname -r) \
+            linux-image-extra-virtual
+sudo apt-get install -y apt-transport-https \
+                           ca-certificates
+sudo curl -fsSL https://yum.dockerproject.org/gpg | sudo apt-key add -
+sudo add-apt-repository \
+           "deb https://apt.dockerproject.org/repo/ \
+                  ubuntu-$(lsb_release -cs) \
+                         main"
+sudo apt-get -y install docker-engine
 
-echo "Installing Docker and Docker Compose (TBD)"
+echo "Now... Docker Compose!"
+sleep 1
+curl -L "https://github.com/docker/compose/releases/download/1.10.0/docker-compose-$(uname -s)-$(uname -m)" -o /usr/local/bin/docker-compose
+chmod +x /usr/local/bin/docker-compose
+
+echo "Checking docker-compose..."
+docker-compose --version
+
+echo "Done!"
+sleep 1
+
+echo "Installing Apache and PHP5"
 sleep 2
-# TODO
+sudo add-apt-repository -y ppa:ondrej/php
+sudo apt-get install -y software-properties-common
+sudo apt-get update
+sudo apt-get install -y php5.6
+sudo apt-get install -y php5.6-mbstring php5.6-mcrypt php5.6-mysql php5.6-xml
 
-echo "Installing Apache and PHP5 (TBD)"
+echo "Checking PHP version..."
+php -v
+
+echo "Installing Java... (Pray to god for your machine)"
+sudo add-apt-repository -y ppa:webupd8team/java
+sudo apt update && sudo apt install -y oracle-java8-installer
+sudo apt install -y oracle-java8-set-default
+
+echo "Installing Maven..."
 sleep 2
-# TODO
-
-echo "Installing Java... (Pray to god for your machine) -> TBD"
-# TODO
-
-echo "Installing Maven... (TBD)"
-sleep 2
-# TODO
-
-echo "Nice! The must-have part is finished. Now we're going to clone the most used agilize repositories to your www folder! (/var/www/html/REPOSITORIES)"
-sleep 2
-echo "Relax, this script also configures the whole clone config stuff."
+sudo apt install -y maven
 
 # Aplicativos que amamos! <3
-# TODO add Terminator, PHPStorm, Eclipse, VSCode, Chrome, Slack, Google Remote
+sudo apt install terminator
+# Still need to figure it out how to install the other ones...
 
-
-zenity --info --title "All configured!" --text "Enjoy the whole thing. :)"
+zenity --info --title "Ready to ROCK!" --text "All must-have programs that we use are now installed. Have a nice coding day!"
