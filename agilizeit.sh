543a82405e1ae81c4b79d3142af3f947fd7873b6
Luiz Nickel <luizaugustonickel@gmail.com>
2017-02-01 09:06:21 -0300

    0.8 - Added Jetbrains Toolbox (Still WIP) / Adds more sleepiness

--- agilizeit.sh
+++ agilizeit.sh
@@ -1,8 +1,7 @@
 #!/bin/bash
 
 # This script builds the whole Agilize environment.
-# Be sure to chmod +x this file and don't forget
-# that this will take time as fuck.
+# Be sure to chmod +x this file :)
 #
 # Enjoy it!
 #
@@ -59,31 +58,55 @@ sudo apt-get update
 sudo apt-get install -y php5.6
 sudo apt-get install -y php5.6-mbstring php5.6-mcrypt php5.6-mysql php5.6-xml
 
-echo " Checking PHP version... "
+echo " Checking PHP version...  "
 php -v
 
-echo "Installing Java... (Pray to god for your machine)"
+echo " Installing Java... (Pray to god for your machine) "
 sudo add-apt-repository -y ppa:webupd8team/java
 sudo apt update && sudo apt install -y oracle-java8-installer
 sudo apt install -y oracle-java8-set-default
 
-echo "Installing Maven... "
+echo " Installing Maven... "
 sleep 2
 sudo apt install -y maven
 
 # Aplicativos que amamos! <3
 
-echo "Installing Terminator... (Not the one from Terminator)"
+echo " Installing Terminator... (Not the one from Terminator) "
 sleep 2
 sudo apt install terminator
 
-echo "Placeholder for Chrome (needs mirror)"
-echo "Placeholder for Slack Desktop (needs mirror)"
-echo "Placeholder for Visual Studio Code (needs mirror)"
-echo "Placeholder for PHPStorm (needs mirror)"
+echo " Downloading and Installing Google Chrome... "
+sleep 2
 
+wget -O Downloads/chrome.deb "https://dl.google.com/linux/direct/google-chrome-stable_current_amd64.deb"
+dpkg -i Downloads/chrome.deb
+
+echo " Downloading and Installing Slack... "
+sleep 2
+
+wget -O Downloads/slack.deb "https://downloads.slack-edge.com/linux_releases/slack-desktop-2.4.2-amd64.deb"
+dpkg -i Downloads/slack.deb
+
+echo " Downloading and Installing Visual Studio Code... "
+sleep 2
+
+wget -O Downloads/code.deb "https://az764295.vo.msecnd.net/stable/ee428b0eead68bf0fb99ab5fdc4439be227b6281/code_1.8.1-1482158209_amd64.deb"
+dpkg -i Downloads/code.deb
+
+# WIP (need testing)
+echo "Downloading and Installing Jetbrains Toolbox (So you can download their IDEs, like IDEA and PHPStorm)"
+wget -O Downloads/toolbox.tar.gz "https://dl.dropboxusercontent.com/content_link/66lzGkamsrt3fa9zuVpNbsfSoo2BGelptVlbmxT2jaWxdd4pKdmias3otFQlcWVD/file?dl=1"
+tar -xvf Downloads/toolbox.tar.gz
+cd toolbox
+chmod +x toolbox
+echo "Starting Toolbox..."
+sleep 2
+./toolbox
+
+# Gonna directly install it via crx, probably on next release
 echo "About Google Remote Desktop... You'll have to install it by yourself. Lemme open you the link for downloading it. "
-sleep 1
+sleep 4
 python -mwebbrowser https://goo.gl/YFNOCF
 
 zenity --info --title "Ready to ROCK!" --text "All must-have programs that we use are now installed. Have a nice coding day!"
