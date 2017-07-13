ab9292ea810ace9d26c57a0ee1debd74445cdf10
Luiz Nickel <luizaugustonickel@gmail.com>
2017-07-13 09:35:06 -0300

    Corrige tree

--- install.sh
+++ install.sh
@@ -7,5 +7,5 @@
 # Configurando script pro sistema
 git clone https://github.com/vaporwavie/agilizeit.sh.git ~/.agilizeit
 echo "alias agilizeit='~/.agilizeit/.agilizeit/agilizeit.sh'" >> ~/.zshrc
-echo "alias agilizeit-update='cd ~/.agilizeit && git pull https://github.com/vaporwavie/agilizeit.sh.git'" >> ~/.zshrc
+echo "alias agilizeitupdate='cd ~/.agilizeit && git pull https://github.com/vaporwavie/agilizeit.sh.git'" >> ~/.zshrc
 zenity --info --title "Agilize it - Instalação" --text "<big>O agilize it foi instalado!</big>\nExecute ele digitando 'agilizeit' no seu terminal." --width=300 --height=200 2> /dev/null
