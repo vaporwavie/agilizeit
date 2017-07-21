9f3f7e0db01fdf64f8ec2ab926ff54df38a3cec5
Thiago Colares <thicolares@gmail.com>
2017-07-20 23:28:05 -0300

    Faz source do .zshrc durante instalação
    Senti falta disso durante instalação no Ubuntu 16.04

--- install.sh
+++ install.sh
@@ -9,4 +9,5 @@
 git clone https://github.com/vaporwavie/agilizeit.sh.git ~/.agilizeit
 echo "alias agilizeit='~/.agilizeit/.agilizeit/agilizeit.sh'" >> ~/.zshrc
 echo "alias agilizeitupdate='cd ~/.agilizeit && git pull https://github.com/vaporwavie/agilizeit.sh.git'" >> ~/.zshrc
+source ~/.zshrc
 zenity --info --title "Agilize it - Instalação" --text "<big>O agilize it foi instalado!</big>\nExecute ele digitando 'agilizeit' no seu terminal." --width=300 --height=200 2> /dev/null
